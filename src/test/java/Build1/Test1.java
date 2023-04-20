a914c7dc2e081fe6518541afee51c3626806f4fc
johnwb1996 <117428487+johnwb1996@users.noreply.github.com>
2023-04-20 21:34:03 +0530

    commit1

---
+++ src/test/java/Build1/Test1.java
@@ -0,0 +1,14 @@
+package Build1;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	
+	@Test
+	public void newT()
+	{
+		System.out.println("Run1");
+	}
+	
+}
---
+++ src/test/java/Build1/Test2.java
@@ -0,0 +1,12 @@
+package Build1;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void newT2()
+	{
+		System.out.println("Run2");
+	}
+
+}
